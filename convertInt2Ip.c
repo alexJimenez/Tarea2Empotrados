421deb2856f08f47282b283260fde4f4051dc9d8
Alex Jimenez <jimenez.00@gmail.com>
2011-05-11 12:54:11 -0600

    Get the value and convert

--- convertInt2Ip.c
+++ convertInt2Ip.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 
 void main(){
-	int num2Convert;
-	printf("Ingrese el un numero entero:");
-	scanf("%d",num2Convert);
-	printf("%d",(num2Convert >> 24));
+	unsigned int num2Convert, fisrtOct, secondOct, thirdOct, lastOct;	
+	printf("Ingrese el un numero a convertir:");
+	scanf("%u",&num2Convert);
+	fisrtOct = num2Convert >> 24;
+	secondOct = (num2Convert << 8 ) >> 24;
+	thirdOct = (num2Convert << 16 ) >> 24;
+	lastOct = (num2Convert << 24 ) >> 24;
+	printf("Ip: %u.%u.%u.%u\n", fisrtOct, secondOct, thirdOct, lastOct);
 }
