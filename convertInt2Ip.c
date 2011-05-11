3b36400637c938075101f55253ff87f72190e6ce
Alex Jimenez <jimenez.00@gmail.com>
2011-05-10 22:29:18 -0600

    Adding functionality

--- convertInt2Ip.c
+++ convertInt2Ip.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
 void main(){
-	printf("First test");
-	
+	int num2Convert;
+	printf("Ingrese el un numero entero:");
+	scanf("%d",num2Convert);
+	printf("%d",(num2Convert >> 24));
 }
